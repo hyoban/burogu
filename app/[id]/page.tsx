5f085602e9e0b1fe3901cc7be0bf685e9c5f43f6
hyoban <hi@hyoban.cc>
2023-01-17 21:23:59 +0800

    fix: handle post id not found

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,8 +1,13 @@
 import './prose.css'
 
-import { getPost } from '@/lib/notion'
+import { getPost, getPosts } from '@/lib/notion'
+
+const databaseId = process.env.NOTION_DATABASE_ID as string
 
 export default async function Page({ params }: { params: { id: string } }) {
+  const posts = await getPosts(databaseId)
+  if (!posts.find((post) => post.id === params.id))
+    return <div>Post not found</div>
   const page = await getPost(params.id)
   return (
     <div className="prose mb-10">
