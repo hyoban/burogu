517be282d9023fcedc4bb9e6841b562a6949eaaa
hyoban <hi@hyoban.cc>
2023-01-20 09:36:14 +0800

    chore: move site info to config.json

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { Suspense } from 'react'
 
 import ApperanceSwitch from '@/app/components/AppearanceSwitch'
 import Icon from '@/app/icons/Icon'
-import profilePic from '@/public/hyoban.png'
 
+import config from '../siteconfig.json'
 import PostList from './components/PostList'
 import Image from 'next/image'
 
@@ -16,32 +16,38 @@ export default async function Home({}) {
         <div className="flex items-center space-x-4">
           <Image
             className="w-16 h-16 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
-            src={profilePic}
+            src={config.avatarPath}
             alt=""
+            width={64}
+            height={64}
           />
           <div className="font-medium dark:text-white">
-            <div className="text-xl mb-1">Hyoban</div>
+            <div className="text-xl mb-1">{config.siteName}</div>
             <div className="text-sm text-gray-500 dark:text-gray-400">
-              Student | Love react
+              {config.shortDescription}
             </div>
           </div>
         </div>
       </header>
-      <p className="mt-8">
-        My name is Stephen Zhou ( 周云亮 in Chinese ), Hyoban is my ID on the
-        web. I&apos;m studying at NCUT.
-      </p>
+      <p className="mt-8">{config.fullDescription}</p>
       <p className="mt-4">
         <span>Find me on </span>
-        <a href="https://github.com/hyoban" target="_blank" rel="noreferrer">
-          <Icon className="i-carbon-logo-github" />
-        </a>
-        <a
-          href="https://elk.zone/mas.to/@hyoban"
-          target="_blank"
-          rel="noreferrer">
-          <Icon className="i-mdi-mastodon" />
-        </a>
+        {config.links.map((link) => {
+          switch (link.type) {
+            case 'GitHub':
+              return (
+                <a href={link.url} target="_blank" rel="noreferrer">
+                  <Icon className="i-carbon-logo-github" />
+                </a>
+              )
+            case 'Mastodon':
+              return (
+                <a href={link.url} target="_blank" rel="noreferrer">
+                  <Icon className="i-mdi-mastodon" />
+                </a>
+              )
+          }
+        })}
       </p>
       <article className="flex flex-col w-full gap-6 my-8">
         <Suspense fallback={<div>Loading post list...</div>}>
