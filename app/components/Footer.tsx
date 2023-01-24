66efe0be310da5135022eef5ade29c26af97a9ab
hyoban <hi@hyoban.cc>
2023-01-24 11:27:57 +0800

    chore: move footer out

---
+++ app/components/Footer.tsx
@@ -0,0 +1,25 @@
+import Icon from '../icons/Icon'
+import ApperanceSwitch from './AppearanceSwitch'
+
+export default function Footer() {
+  return (
+    <footer className="mx-auto my-10 flex flex-col items-center gap-4 font-mono opacity-60">
+      <p>
+        Powered by <a href="https://beta.nextjs.org/">Next.js</a> and{' '}
+        <a href="https://tailwindcss.com/">Tailwind CSS</a>
+      </p>
+      <div className="flex items-center justify-center">
+        <ApperanceSwitch />
+        <a
+          href="https://github.com/hyoban/blog-next-notion"
+          target="_blank"
+          rel="noreferrer">
+          <Icon className="i-carbon-logo-github"></Icon>
+        </a>
+        <a href="/feed" target="_blank" rel="noreferrer">
+          <Icon className="i-carbon-rss"></Icon>
+        </a>
+      </div>
+    </footer>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
-import ApperanceSwitch from './components/AppearanceSwitch'
-import Link from 'next/link'
-
 import './css/globals.css'
 
-import Icon from './icons/Icon'
 import Provider from './provider'
+import Footer from './components/Footer'
 
 export default function RootLayout({
   children,
@@ -20,21 +17,8 @@ export default function RootLayout({
       <head />
 
       <body className="p-6 font-sans dark:bg-[#121212] dark:text-white">
-        <Provider>
-          {children}
-          <footer className="mx-auto my-10 flex items-center justify-center">
-            <ApperanceSwitch />
-            <a
-              href="https://github.com/hyoban/blog-next-notion"
-              target="_blank"
-              rel="noreferrer">
-              <Icon className="i-carbon-logo-github"></Icon>
-            </a>
-            <a href="/feed" target="_blank" rel="noreferrer">
-              <Icon className="i-carbon-rss"></Icon>
-            </a>
-          </footer>
-        </Provider>
+        <Provider>{children}</Provider>
+        <Footer />
       </body>
     </html>
   )
