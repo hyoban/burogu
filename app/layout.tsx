6581fac93b9bc6eeb4a0af3fdf1a9a08a58dab6c
hyoban <hi@hyoban.cc>
2023-01-29 11:48:25 +0800

    fix: use h-full instead of h-screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-import Script from 'next/script'
-
 import Provider from './provider'
 import Footer from './components/Footer'
 
@@ -11,14 +9,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="h-full">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
 
-      <body className="flex min-h-screen justify-center p-6 font-sans dark:bg-[#121212] dark:text-white">
+      <body className="flex h-auto min-h-full justify-center p-6 font-sans dark:bg-[#121212] dark:text-white">
         <Provider>
           <div className="flex w-full max-w-[70ch] flex-col justify-between">
             {children}
@@ -26,25 +24,6 @@ export default function RootLayout({
           </div>
         </Provider>
       </body>
-
-      <Script id="postcss-viewport-height-correction">
-        {`var customViewportCorrectionVariable = 'vh';
-function setViewportProperty(doc) {
-var prevClientHeight;
-var customVar = '--' + ( customViewportCorrectionVariable || 'vh' );
-function handleResize() {
-    var clientHeight = window.innerHeight;
-    if (clientHeight === prevClientHeight) return;
-    requestAnimationFrame(function updateViewportHeight(){
-        doc.style.setProperty(customVar, (clientHeight * 0.01) + 'px');
-        prevClientHeight = clientHeight;
-    });
-}
-handleResize();
-    return handleResize;
-}
-window.addEventListener('resize', setViewportProperty(document.documentElement));`}
-      </Script>
     </html>
   )
 }
