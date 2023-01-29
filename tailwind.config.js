0936dae9b11586f716a00dd184f214ae1fa659ab
hyoban <hi@hyoban.cc>
2023-01-29 21:54:33 +0800

    chore: update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './app/**/*.{js,ts,jsx,tsx}', // Note the addition of the `app` directory.
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './icons/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
