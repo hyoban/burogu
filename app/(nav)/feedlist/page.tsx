ceb6cdf7ef079c60eaeaf39dbae21de6bec292a4
hyoban <hi@hyoban.cc>
2023-01-30 18:40:17 +0800

    fix: revalidate config for rss feed list

--- app/(nav)/feedlist/page.tsx
+++ app/(nav)/feedlist/page.tsx
@@ -2,6 +2,8 @@ import { Suspense } from 'react'
 
 import FeedList from '@/app/components/FeedList'
 
+export const revalidate = 3600
+
 export default function FeedListPage({}) {
   return (
     <Suspense fallback={<div>Loading my feed list...</div>}>
