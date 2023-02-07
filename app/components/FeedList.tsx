b06dc36675126f8460618056934c06a90f09e8e6
hyoban <hi@hyoban.cc>
2023-02-07 16:22:52 +0800

    chore: remove select for rss type

--- app/components/FeedList.tsx
+++ app/components/FeedList.tsx
@@ -1,53 +1,25 @@
-'use client'
-
 import { FeedListType } from '@/lib/notion'
 import { timeZone } from '@/site.config.cjs'
 import dayjs from 'dayjs'
 import timezone from 'dayjs/plugin/timezone'
 import utc from 'dayjs/plugin/utc'
-import { useState } from 'react'
 
 dayjs.extend(utc)
 dayjs.extend(timezone)
 dayjs.tz.setDefault(timeZone)
 
 export default function FeedList({ feedList }: { feedList: FeedListType }) {
-  const feedTypes = ['All'].concat(
-    feedList
-      .map((feed) => feed.feedInfo.type)
-      .filter((v, i, a) => a.indexOf(v) == i),
-  )
-
-  const [selectedFeedType, setSelectedFeedType] = useState('All')
-
-  const feedListGroupedByYear = feedList
-    .filter(
-      (feed) =>
-        selectedFeedType === 'All' || feed.feedInfo.type === selectedFeedType,
-    )
-    .reduce((acc, feed) => {
-      const feedYear = dayjs(feed.isoDate).tz(timeZone).format('YYYY')
-      if (!acc[feedYear]) {
-        acc[feedYear] = []
-      }
-      acc[feedYear].push(feed)
-      return acc
-    }, {} as Record<string, typeof feedList>)
+  const feedListGroupedByYear = feedList.reduce((acc, feed) => {
+    const feedYear = dayjs(feed.isoDate).tz(timeZone).format('YYYY')
+    if (!acc[feedYear]) {
+      acc[feedYear] = []
+    }
+    acc[feedYear].push(feed)
+    return acc
+  }, {} as Record<string, typeof feedList>)
 
   return (
     <>
-      <div className="flex flex-row items-center gap-3">
-        <span className="text-gray-500">Filter by type:</span>
-        <select
-          className="rounded-md border border-gray-300"
-          onChange={(e) => setSelectedFeedType(e.target.value)}>
-          {feedTypes.map((feedType) => (
-            <option key={feedType} value={feedType}>
-              {feedType}
-            </option>
-          ))}
-        </select>
-      </div>
       {Object.keys(feedListGroupedByYear)
         .sort((a, b) => Number(b) - Number(a))
         .map((feedYear) => {
