bcf0b0ba733bc0d92d90ec5146fcfeb216bfd3e5
hyoban <hi@hyoban.cc>
2023-03-21 16:26:46 +0800

    feat: add avatar component

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import config from "@/site.config.cjs";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+    <motion.a
+      href={config.authorLink}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="hover:opacity-100"
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+      transition={{ type: "spring", stiffness: 400, damping: 17 }}
+    >
+      <Image
+        className="h-16 w-16 rounded-full"
+        src={config.avatarPath}
+        alt={config.authorName}
+        width={64}
+        height={64}
+      />
+    </motion.a>
+  );
+};
+
+export default Avatar;
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,26 +1,13 @@
+import Avatar from "@/app/components/Avatar";
 import Nav from "@/app/components/Nav";
 import config from "@/site.config.cjs";
-import Image from "next/image";
 
 export default function Header({}) {
   return (
     <header className="flex w-full items-center justify-between gap-6">
       <div className="flex items-center space-x-4">
-        <a
-          href={config.authorLink}
-          target="_blank"
-          className="hover:opacity-100"
-          rel="noreferrer"
-        >
-          <Image
-            className="h-16 w-16 rounded-full p-1 ring-2 ring-gray-300 dark:ring-gray-500"
-            src={config.avatarPath}
-            alt=""
-            width={64}
-            height={64}
-          />
-        </a>
-        <div className=" hidden font-medium dark:text-white sm:block">
+        <Avatar />
+        <div className="hidden font-medium dark:text-white sm:block">
           <div className="mb-1 text-xl">{config.authorName}</div>
           <div className="text-sm text-gray-500 dark:text-gray-400">
             {config.shortDescription}
