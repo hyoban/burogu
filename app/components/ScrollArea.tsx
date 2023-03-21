b97c6aebb0a654f4ac9a4b1bcabce24a1d8fd813
hyoban <hi@hyoban.cc>
2023-03-21 19:53:51 +0800

    fix: component color

--- app/components/ScrollArea.tsx
+++ app/components/ScrollArea.tsx
@@ -39,7 +39,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-slate-300 dark:bg-slate-700" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-neutral-300 dark:bg-neutral-700" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ));
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
