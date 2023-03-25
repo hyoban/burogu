ff626ea6dca1ba499fd4ba24e94a3facf9c46dae
hyoban <hi@hyoban.cc>
2023-03-25 13:06:07 +0800

    fix: tailwind for mdx

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
-	],
+	content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
 	theme: {
 		extend: {
 			fontFamily: {
