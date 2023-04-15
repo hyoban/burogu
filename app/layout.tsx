29fc2806eb4d79f6a5908be82d98a08dcb90ec36
hyoban <hi@hyoban.cc>
2023-04-15 18:44:08 +0800

    chore: move analytics back

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "@/app/css/globals.css"
 import Footer from "@/app/components/part/Footer"
 import { ThemeProvider } from "@/app/provider"
 import config from "@/site.config.cjs"
+import { Analytics } from "@vercel/analytics/react"
 import dayjs from "dayjs"
 import timezone from "dayjs/plugin/timezone"
 import utc from "dayjs/plugin/utc"
@@ -98,6 +99,7 @@ export default function RootLayout({
 						<Footer className="mt-6 print:hidden" />
 					</div>
 				</ThemeProvider>
+				<Analytics />
 			</body>
 		</html>
 	)
