7fe1d1a12618b27c7736dc190222beaa60104d37
hyoban <hi@hyoban.cc>
2023-04-17 16:43:25 +0800

    fix: nav err

--- lib/static.ts
+++ lib/static.ts
@@ -1,6 +1,6 @@
 export const NAV_LIST = [
 	{ name: "主页", href: "/" },
 	{ name: "在用", href: "/uses" },
-	{ name: "文章", href: "/postlist" },
+	{ name: "文章", href: "/post" },
 	{ name: "订阅列表", href: "/feedlist" },
 ] as const
