fa1427106689d6a9a8bf301a043dd753b17cd965
hyoban <hi@hyoban.cc>
2023-05-13 22:58:33 +0800

    chore: update style

--- app/components/ui/Prose.tsx
+++ app/components/ui/Prose.tsx
@@ -8,7 +8,7 @@ const Prose = (props: BasicProps) => {
 			className={cn(
 				"flex flex-col gap-4 relative",
 				"prose prose-neutral dark:prose-invert",
-				"prose-h1:my-0 prose-h2:my-0 prose-h3:my-0 prose-h4:my-0 prose-h5:my-0 prose-h6:my-0",
+				"prose-h1:my-4 prose-h2:my-2 prose-h3:my-0 prose-h4:my-0 prose-h5:my-0 prose-h6:my-0",
 				"prose-ul:my-0 prose-ol:my-0 prose-p:my-0",
 				"prose-a:underline-offset-4",
 				className
