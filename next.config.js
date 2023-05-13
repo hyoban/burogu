945b146199168e68c6be436016b86976ef190f5a
hyoban <hi@hyoban.cc>
2023-05-13 15:11:30 +0800

    chore: disable static export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: "export",
-	images: {
-		unoptimized: true,
+	experimental: {
+		serverComponentsExternalPackages: ["shiki", "vscode-oniguruma"],
 	},
 }
 
