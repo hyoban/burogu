2281d4c34d50b84dd4ed598db1856e93624f5de2
Stephen Zhou <hi@hyoban.cc>
2023-05-20 19:00:58 +0800

    chore: update

--- app/post/[id]/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-	return (
-		<div className="my-4 flex animate-pulse flex-col gap-4">
-			<p className="h-8 w-96 rounded-md bg-neutral-200 dark:bg-neutral-700"></p>
-			<p className="h-12 w-full rounded-md bg-neutral-200 dark:bg-neutral-700"></p>
-		</div>
-	)
-}
